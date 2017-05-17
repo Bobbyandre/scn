b9c0efbc280d008ab07eb2d3709319a098d3a60c
Bobbyandre <bobstoragethree@gmail.com>
2017-05-17 23:43:16 +0200

    Add files via upload

---
+++ js/indexx.js
@@ -0,0 +1,19 @@
+function wait(){
+
+  var logBtn = document.getElementById("goLog");
+  var signBtn = document.getElementById("goSign");
+
+logBtn.addEventListener('click',function(){
+
+  window.location.href = "file:///home/bo/WebstormProjects/app101/www/log.html";
+
+},false);
+
+
+signBtn.addEventListener('click',function(){
+
+  window.location.href = "file:///home/bo/WebstormProjects/app101/www/log.html";
+
+},false);
+
+}
